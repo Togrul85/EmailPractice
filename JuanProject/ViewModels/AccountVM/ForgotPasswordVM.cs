f7d604dd3116221b488f263408e424a3bd4682dd
Togrul85 <toghrulkhs@code.edu.az>
2023-03-23 00:11:50 +0400

    JuanTemplate

---
+++ JuanProject/ViewModels/AccountVM/ForgotPasswordVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JuanProject.ViewModels.AccountVM
+{
+    public class ForgotPasswordVM
+    {
+        [Required]
+
+        [DataType(DataType.EmailAddress)]
+
+        public string Email { get; set; }
+    }
+}
